e8d1d1cb50a20279a277a5fd27a1f6fa64a6adc0
Bruno <71056568+brunoo-p@users.noreply.github.com>
2021-05-11 18:39:27 -0300

    Scroll Chat

--- chatapp/src/Components/ChatWindow/index.jsx
+++ chatapp/src/Components/ChatWindow/index.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Message from '../Message';
 import { Container } from './styles';
 
 export default function ChatWindow({chat, myUser}) {
+    
+    const messageElement = useRef(null);
+
+    useEffect(() => {
+        if (messageElement) {
+            messageElement.current.addEventListener('DOMNodeInserted', event => {
+            const { currentTarget: target } = event;
+            setTimeout(() => {
+
+                target.scroll({ top: target.scrollHeight, behavior: 'smooth' });
+            }, 300);
+          });
+        }
+      }, []);
+    
     
     const conversa = chat.map(msg =>
         <Message key={Date.now() * Math.random()}
@@ -13,7 +28,7 @@ export default function ChatWindow({chat, myUser}) {
 
         
     return (
-        <Container>
+        <Container ref={messageElement}>
             {conversa}        
         </Container>
     )
