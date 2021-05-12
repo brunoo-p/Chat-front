b4d347af246a9fa2c2ac71f431a7fbf1c6c73ad2
Bruno <71056568+brunoo-p@users.noreply.github.com>
2021-05-12 17:52:12 -0300

    Refactoring UI

--- chatapp/src/Components/ChatInput/styles.js
+++ chatapp/src/Components/ChatInput/styles.js
@@ -39,33 +39,60 @@ export const WriteMessage = styled.div`
     justify-content: space-between;
     padding: 5px;
 
-    .inputSubmit{
-        width: 80px;
-        margin-left: 10px;
-        padding: 5px;
-        cursor: pointer;
-        background: transparent;
-        border: 2px solid rgb(64, 115, 158, .7);
-        color: #353b48;
-        font-size: 15px;
-        font-weight: 700;
-        position: relative; 
-    }
+  `;
+export const SendMessage = styled.button`
+    width: 80px;
+
+    margin-top: 5px;
+    margin-left: 10px;
+    padding: 5px;
+    
+    cursor: pointer;
+    
+    background: transparent;
+    border: 2px solid #60a3bc;
+    border-radius: 8px;
+    color: #353b48;
+    
+    font-size: 15px;
+    font-weight: 700;
+    position: relative;
     
     ::before {
         content: '';
+
         position: absolute;
-        margin-left: 327px;
-        border-radius: 5px;
-        margin-top: 7px;
-        background: linear-gradient(0.45turn, rgba(64, 115, 158, .7), rgba(253, 150, 68, .5));
         width: 0;
         height: 41px;
-        transition: .3s ease-in;
+        margin-left: -7px;
+        margin-top: -12px;
+
+        border-radius: 5px;
+        background: linear-gradient(0.45turn, rgba(64, 115, 158, .7), rgba(253, 150, 68, .5));
+        
+
+        color: #60a3bc;
+        transition: .2s ease-in;
     }
 
-    :hover::before{
+    &:hover::before{
         width: 61px;
+    }
+
+`;
+
+export const MicIcon = styled(FaMicrophoneAlt)`
+    width: 28px; 
+    height: 28px;
+    margin: 15px 5px 0 10px;
+    padding: 0 10px;
+    cursor: pointer;
+    transition: 0.2s ease-in;
+    :hover{
+        color: #60a3bc;
+    }
+    :focus{
+        color: #60a3bc;
     }
 
 `;
